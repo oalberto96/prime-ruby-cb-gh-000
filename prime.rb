3237b8ec0738a5d587fc38568b6e5ae4126f3cf6
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 01:52:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 def prime?(number)
+  primes = []
   range = (2..number).to_a
-  range.any? {|e| (e % number == 0) && e == number  }
+  range.any? {|e|  (e % number == 0) && e == number  }
 end
